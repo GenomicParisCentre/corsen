2c9a052749f20a7a70aea566670442357a2ca961
Laurent Jourdren <jourdren@biologie.ens.fr>
2006-11-22 16:19:37 +0000

    Homogeneisation of keys names Add new settings to define the parameter of the 3D visualisation

--- src/main/java/Settings.java
+++ src/main/java/Settings.java
@@ -1,3 +1,4 @@
+import java.awt.Color;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -29,20 +30,33 @@ import java.util.Properties;
 
 public final class Settings {
 
-  private static final String ZFACTOR_KEY = "zFactor";
-  private static final String FACTOR_KEY = "factor";
-  private static final String UNIT_KEY = "unit";
-  private static final String AUTO_CUBOID_SIZE_KEY = "autoCuboidSize";
-  private static final String CUBOID_SIZE_KEY = "cuboidSize";
-  private static final String SAVE_DATA_FILE_KEY = "saveDataFile";
-  private static final String SAVE_IV_FILE_SAVED = "saveIVFile";
-  private static final String SAVE_3DFILES_KEY = "save3dFiles";
-  private static final String SAVE_MESSENGERS_3DFILES_KEY = "saveMessengers3dFile";
-  private static final String SAVE_MESSENGERS_CUBOIDS_3DFILES_KEY = "saveMessengersCuboidsFile";
-  private static final String SAVE_MITOS_3DFILES_KEY = "saveMitos3DFile";
-  private static final String SAVE_MITOS_CUBOIDS_3DFILES_KEY = "saveMitosCuboids3dFile";
-  private static final String SAVE_DISTANCES_3DFILES_KEY = "saveDistances3dFile";
-  private static final String SAVE_FULL_RESULTS_KEY = "saveFullResultsFile";
+  private static final String ZFACTOR_KEY = "main.zFactor";
+  private static final String FACTOR_KEY = "main.factor";
+  private static final String UNIT_KEY = "main.unit";
+  private static final String AUTO_CUBOID_SIZE_KEY = "main.autoCuboidSize";
+  private static final String CUBOID_SIZE_KEY = "main.cuboidSize";
+  private static final String SAVE_DATA_FILE_KEY = "save.dataFile";
+  private static final String SAVE_IV_FILE_SAVED = "save.IVFile";
+  private static final String SAVE_3DFILES_KEY = "save.3dFiles";
+  private static final String SAVE_MESSENGERS_3DFILES_KEY = "save.3dFiles.messengers";
+  private static final String SAVE_MESSENGERS_CUBOIDS_3DFILES_KEY = "save.3dFiles.messengersCuboids";
+  private static final String SAVE_MITOS_3DFILES_KEY = "save.3dFiles.mitos";
+  private static final String SAVE_MITOS_CUBOIDS_3DFILES_KEY = "save.3dFiles.mitosCuboids";
+  private static final String SAVE_DISTANCES_3DFILES_KEY = "save.3dFiles.distances";
+  private static final String SAVE_FULL_RESULTS_KEY = "save.fullResults";
+
+  private static final String VISUALIZATION_POINTS_SIZE_KEY = "visualization.pointSize";
+  private static final String VISUALIZATION_SHOW_SURFACE_LINES_KEY = "visualization.showSurfaceLines";
+  private static final String VISUALIZATION_SURFACE_LINE_SIZE_KEY = "visualization.surfaceLinesSize";
+  private static final String VISUALIZATION_DISTANCES_LINES_SIZE_KEY = "visualization.distanceLinesSize";
+  private static final String VISUALIZATION_SHOW_NEGATIVE_DISTANCES_KEY = "visualization.showNegativeDistances";
+
+  private static final String VISUALIZATION_COLOR_MESSENGERS_KEY = "visualization.color.messengers";
+  private static final String VISUALIZATION_COLOR_MITOS_KEY = "visualization.color.mitos";
+  private static final String VISUALIZATION_COLOR_BARYCENTERS_KEY = "visualization.color.barycenters";
+  private static final String VISUALIZATION_COLOR_DISTANCES_KEY = "visualization.color.distances";
+  private static final String VISUALIZATION_COLOR_BACKGROUND_KEY = "visualization.color.background";
+  private static final String VISUALIZATION_COLOR_LEGEND_KEY = "visualization.color.legend";
 
   private Properties properties = new Properties();
 
@@ -100,7 +114,8 @@ public final class Settings {
    */
   public boolean isSaveDistances3dFile() {
 
-    String value = this.properties.getProperty(SAVE_DISTANCES_3DFILES_KEY, "true");
+    String value = this.properties.getProperty(SAVE_DISTANCES_3DFILES_KEY,
+        "true");
 
     return Boolean.valueOf(value.trim());
   }
@@ -122,7 +137,8 @@ public final class Settings {
    */
   public boolean isSaveMessengers3dFile() {
 
-    String value = this.properties.getProperty(SAVE_MESSENGERS_3DFILES_KEY, "true");
+    String value = this.properties.getProperty(SAVE_MESSENGERS_3DFILES_KEY,
+        "true");
 
     return Boolean.valueOf(value);
   }
@@ -133,8 +149,8 @@ public final class Settings {
    */
   public boolean isSaveMessengersCuboids3dFile() {
 
-    String value = this.properties.getProperty(SAVE_MESSENGERS_CUBOIDS_3DFILES_KEY,
-        "false");
+    String value = this.properties.getProperty(
+        SAVE_MESSENGERS_CUBOIDS_3DFILES_KEY, "false");
 
     return Boolean.valueOf(value.trim());
   }
@@ -204,10 +220,142 @@ public final class Settings {
     return Float.parseFloat(value.trim());
   }
 
+  /**
+   * Get the size of the points in vizualisations.
+   * @return The size of the points
+   */
+  public float getVisualizationPointsSize() {
+
+    String value = this.properties.getProperty(VISUALIZATION_POINTS_SIZE_KEY,
+        "1.0");
+
+    return Float.parseFloat(value.trim());
+  }
+
+  /**
+   * Get the size of the lines in vizualisations.
+   * @return The size of the points
+   */
+  public float getVisualizationSurfaceLinesSize() {
+
+    String value = this.properties.getProperty(
+        VISUALIZATION_SURFACE_LINE_SIZE_KEY, "1.0");
+
+    return Float.parseFloat(value.trim());
+  }
+
+  /**
+   * Get the size of the distances lines in vizualisations.
+   * @return The size of the points
+   */
+  public float getVisualizationDistancesLinesSize() {
+
+    String value = this.properties.getProperty(
+        VISUALIZATION_DISTANCES_LINES_SIZE_KEY, "1.0");
+
+    return Float.parseFloat(value.trim());
+  }
+
+  /**
+   * Test if surface lines must be show in visualizations.
+   * @return true if surface lines must be show in visualizations
+   */
+  public boolean isVisualizationShowSurfaceLines() {
+
+    String value = this.properties.getProperty(
+        VISUALIZATION_SHOW_SURFACE_LINES_KEY, "true");
+
+    return Boolean.valueOf(value.trim());
+  }
+
+  /**
+   * Test if the distances lines must be shown in a different color if negative.
+   * @return trueif the distances lines must be shown in a different color if
+   *         negative
+   */
+  public boolean isVisualizationShowNegativeDistances() {
+
+    String value = this.properties.getProperty(
+        VISUALIZATION_SHOW_NEGATIVE_DISTANCES_KEY, "false");
+
+    return Boolean.valueOf(value.trim());
+  }
+
+  /**
+   * Get the color of messengers
+   * @return The color of the messengers
+   */
+  public Color getColorMessengers() {
+
+    String value = this.properties.getProperty(
+        VISUALIZATION_COLOR_MESSENGERS_KEY, colorToString(Color.GREEN));
+
+    return colorFromString(value);
+  }
+
+  /**
+   * Get the color of mitos
+   * @return The color of the mitos
+   */
+  public Color getColorMitos() {
+
+    String value = this.properties.getProperty(VISUALIZATION_COLOR_MITOS_KEY,
+        colorToString(Color.RED));
+
+    return colorFromString(value);
+  }
+
+  /**
+   * Get the color of barycenters
+   * @return The color of the barycenter
+   */
+  public Color getColorBaryCenters() {
+
+    String value = this.properties.getProperty(
+        VISUALIZATION_COLOR_BARYCENTERS_KEY, colorToString(Color.BLUE));
+
+    return colorFromString(value);
+  }
+
+  /**
+   * Get the color of distances
+   * @return The color of the distances
+   */
+  public Color getColorDistances() {
+
+    String value = this.properties.getProperty(
+        VISUALIZATION_COLOR_DISTANCES_KEY, colorToString(Color.YELLOW));
+
+    return colorFromString(value);
+  }
+
+  /**
+   * Get the color of the background
+   * @return The color of background
+   */
+  public Color getColorBackground() {
+
+    String value = this.properties.getProperty(
+        VISUALIZATION_COLOR_BACKGROUND_KEY, colorToString(Color.BLACK));
+
+    return colorFromString(value);
+  }
+  
+  /**
+   * Get the color of the legend
+   * @return The color of the legend
+   */
+  public Color getColorLegend() {
+
+    String value = this.properties.getProperty(
+        VISUALIZATION_COLOR_LEGEND_KEY, colorToString(Color.WHITE));
+
+    return colorFromString(value);
+  }
+
   //
   // Setters
   //
-
   /**
    * Set if the iv file must be saved.
    * @param saveIVFile true if the iv file must be saved
@@ -270,7 +418,8 @@ public final class Settings {
    * Set if 3d messengers cuboids file must be saved.
    * @param messengersCuboids3dFile The messengersCuboids3dFile to set
    */
-  public void setSaveMessengersCuboids3dFile(final boolean messengersCuboids3dFile) {
+  public void setSaveMessengersCuboids3dFile(
+      final boolean messengersCuboids3dFile) {
 
     this.properties.setProperty(SAVE_MESSENGERS_CUBOIDS_3DFILES_KEY, Boolean
         .toString(messengersCuboids3dFile));
@@ -282,7 +431,8 @@ public final class Settings {
    */
   public void setSaveMito3dFile(final boolean mito3dFile) {
 
-    this.properties.setProperty(SAVE_MITOS_3DFILES_KEY, Boolean.toString(mito3dFile));
+    this.properties.setProperty(SAVE_MITOS_3DFILES_KEY, Boolean
+        .toString(mito3dFile));
   }
 
   /**
@@ -342,9 +492,157 @@ public final class Settings {
         .toString(saveFullResultFile));
   }
 
+  /**
+   * Set the size of the points in vizualisations.
+   * @param size The size of the points
+   */
+  public void setVisualizationPointsSize(final float size) {
+
+    this.properties.setProperty(VISUALIZATION_POINTS_SIZE_KEY, Float
+        .toString(size));
+  }
+
+  /**
+   * Set the size of the lines in vizualisations.
+   * @param size The size of the points
+   */
+  public void setVisualizationSurfaceLinesSize(final float size) {
+
+    this.properties.setProperty(VISUALIZATION_SURFACE_LINE_SIZE_KEY, Float
+        .toString(size));
+  }
+
+  /**
+   * Set the size of the distances lines in vizualisations.
+   * @param size The size of the points
+   */
+  public void setVisualizationDistancesLinesSize(final float size) {
+
+    this.properties.setProperty(
+        VISUALIZATION_DISTANCES_LINES_SIZE_KEY, Float.toString(size));
+  }
+
+  /**
+   * Set if surface lines must be show in visualizations.
+   * @param value if surface lines must be show in visualizations
+   */
+  public void setVisualizationShowSurfaceLines(final boolean value) {
+
+    this.properties.setProperty(VISUALIZATION_SHOW_SURFACE_LINES_KEY, Boolean
+        .toString(value));
+
+  }
+
+  /**
+   * Set if the distances lines must be shown in a different color if negative.
+   * @param value if the distances lines must be shown in a different color if
+   *          negative
+   */
+  public void setVisualizationShowNegativeDistances(final boolean value) {
+
+    this.properties.setProperty(VISUALIZATION_SHOW_NEGATIVE_DISTANCES_KEY,
+        Boolean.toString(value));
+  }
+
+  /**
+   * Set the color of messengers
+   * @param color The color of messengers
+   */
+  public void setColorMessengers(final Color color) {
+
+    this.properties.setProperty(VISUALIZATION_COLOR_MESSENGERS_KEY,
+        colorToString(color));
+
+  }
+
+  /**
+   * Set the color of mitos
+   * @param color The color of mitos
+   */
+  public void setColorMitos(final Color color) {
+
+    this.properties.setProperty(VISUALIZATION_COLOR_MITOS_KEY,
+        colorToString(color));
+  }
+
+  /**
+   * Set the color of barycenters
+   * @param color The color of barycenters
+   */
+  public void setColorBaryCenters(final Color color) {
+
+    this.properties.setProperty(VISUALIZATION_COLOR_BARYCENTERS_KEY,
+        colorToString(color));
+
+  }
+
+  /**
+   * Set the color of distances
+   * @param color The color of the distances
+   */
+  public void setColorDistances(final Color color) {
+
+    this.properties.setProperty(VISUALIZATION_COLOR_DISTANCES_KEY,
+        colorToString(color));
+
+  }
+
+  /**
+   * Set the color of the background
+   * @param color The color of the background
+   */
+  public void setColorBackground(final Color color) {
+
+    this.properties.setProperty(VISUALIZATION_COLOR_BACKGROUND_KEY,
+        colorToString(color));
+  }
+  
+  /**
+   * Set the color of the legend.
+   * @param color The color of the legend
+   */
+  public void setColorLegend(final Color color) {
+
+    this.properties.setProperty(VISUALIZATION_COLOR_LEGEND_KEY,
+        colorToString(color));
+  }
+
   //
   // Other methods
   // 
+
+  public Color colorFromString(final String s) {
+
+    if (s == null)
+      return null;
+
+    String[] words = s.split("\\,");
+
+    if (words.length < 3)
+      return null;
+
+    int r = Integer.parseInt(words[0].trim());
+    int g = Integer.parseInt(words[1].trim());
+    int b = Integer.parseInt(words[2].trim());
+
+    if (words.length > 3) {
+
+      int a = Integer.parseInt(words[3].trim());
+      return new Color(r, g, b, a);
+    }
+
+    return new Color(r, g, b);
+  }
+
+  public String colorToString(final Color c) {
+
+    if (c == null)
+      return null;
+
+    return c.getRed() + "," + c.getGreen() + "," + c.getBlue() + ","
+        + c.getAlpha();
+
+  }
 
   public static String getConfigurationFilePath() {
 
