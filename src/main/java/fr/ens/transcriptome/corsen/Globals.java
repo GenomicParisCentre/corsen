638e506d1bb2b37f94e1e310e4d8431223b9c1fd
Laurent Jourdren <jourdren@biologie.ens.fr>
2007-11-09 09:25:02 +0000

    Now use a copy of the manifest file to get Application version information

--- src/main/java/fr/ens/transcriptome/corsen/Globals.java
+++ src/main/java/fr/ens/transcriptome/corsen/Globals.java
@@ -1,5 +1,9 @@
 package fr.ens.transcriptome.corsen;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /*
  *                      Nividic development code
  *
@@ -24,6 +28,9 @@ package fr.ens.transcriptome.corsen;
 
 public final class Globals {
 
+  private static Properties manifestProperties;
+  private static final String MANIFEST_PROPERTIES_FILE = "/manifest.txt";
+
   /** The name of the application. */
   public static final String APP_NAME = "Corsen";
 
@@ -39,7 +46,7 @@ public final class Globals {
   public static final boolean IS_JAR = isJar();
 
   public static final int THREAD_NUMBER_DEFAULT = 0;
-  
+
   public static final boolean DEBUG = true;
   public static final boolean DEBUG_HOME_DIR = IS_JAR ? false : true;
   public static final boolean SHOW_BUILT = IS_JAR ? true : false;
@@ -48,10 +55,12 @@ public final class Globals {
   public static final float CUBOID_SIZE_FACTOR = 2.1f;
   public static final float PIXEL_SIZE_DEFAULT = 1.0f;
 
-  private static final String WEBSITE_URL_DEFAULT = "http://transcriptome.ens.fr/corsen";
+  private static final String WEBSITE_URL_DEFAULT =
+      "http://transcriptome.ens.fr/corsen";
   public static final String WEBSITE_URL = getWebSiteURL();
   public static final String HANDBOOK_URL = WEBSITE_URL + "/handbook.html";
-  public static final String REPORT_BUG_URL = "mailto:jourdren@biologie.ens.fr?subject=Corsen Report Bug&body=Report here the CorsenSwing bug";
+  public static final String REPORT_BUG_URL =
+      "mailto:jourdren@biologie.ens.fr?subject=Corsen Report Bug&body=Report here the CorsenSwing bug";
 
   public static enum GUI {
     CLI, SWING, QT, FAKE
@@ -65,15 +74,18 @@ public final class Globals {
 
   public static final String EXTENSION_PARTICLES_A_RGL_FILE = "_messengers.R";
 
-  public static final String EXTENSION_PARTICLES_A_CUBOIDS_IV_FILE = "_cuboids.iv";
+  public static final String EXTENSION_PARTICLES_A_CUBOIDS_IV_FILE =
+      "_cuboids.iv";
 
-  public static final String EXTENSION_PARTICLES_B_CUBOIDS_RGL_FILE = "_mitos_cuboids.R";
+  public static final String EXTENSION_PARTICLES_B_CUBOIDS_RGL_FILE =
+      "_mitos_cuboids.R";
 
   public static final String EXTENSION_PARTICLES_B_RGL_FILE = "_mitos.R";
 
   public static final String EXTENSION_PARTICLES_B_IV_FILE = "_mitos.iv";
 
-  public static final String EXTENSION_PARTICLES_A_CUBOIDS_RGL_FILE = "_cuboids.R";
+  public static final String EXTENSION_PARTICLES_A_CUBOIDS_RGL_FILE =
+      "_cuboids.R";
 
   public static final String EXTENSION_DISTANCES_FILE = "_distances.R";
 
@@ -83,58 +95,61 @@ public final class Globals {
 
   public static final String EXTENSION_FULL_RESULT_FILE = ".fullresult";
 
-  public static final String LICENCE_TXT = "This program is developed under the GNU General Public Licence.";
+  public static final String LICENCE_TXT =
+      "This program is developed under the GNU General Public Licence.";
 
-  public static final String ABOUT_TXT = Globals.APP_NAME
-      + " version "
-      + Globals.APP_VERSION
-      + " ("
-      + Globals.APP_BUILD_NUMBER
-      + ")"
-      + " is a software to calc the distances between "
-      + "particles.\n"
-      + "\nThis version has been built on "
-      + APP_BUILD_DATE
-      + ".\n\n"
-      + "Authors:\n"
-      + "  Laurent Jourdren(*) <jourdren@biologie.ens.fr> Main developer, maintener.\n"
-      + "  Mathilde Garcia(**) <garcia@biologie.ens.fr> Project leader, R programming, ImageJ scripting, testing.\n\n"
-      + "(*)  Plate-forme transcriptome, École Normale Supérieure\n"
-      + "(**) Laboratoire de Génétique Moléculaire, École Normale Supérieure\n\n"
-      + "Copyright 2006 École Normale Supérieure.\n" + LICENCE_TXT + "\n";
+  public static final String ABOUT_TXT =
+      Globals.APP_NAME
+          + " version "
+          + Globals.APP_VERSION
+          + " ("
+          + Globals.APP_BUILD_NUMBER
+          + ")"
+          + " is a software to calc the distances between "
+          + "particles.\n"
+          + "\nThis version has been built on "
+          + APP_BUILD_DATE
+          + ".\n\n"
+          + "Authors:\n"
+          + "  Laurent Jourdren(*) <jourdren@biologie.ens.fr> Main developer, maintener.\n"
+          + "  Mathilde Garcia(**) <garcia@biologie.ens.fr> Project leader, R programming, ImageJ scripting, testing.\n\n"
+          + "(*)  Plate-forme transcriptome, École Normale Supérieure\n"
+          + "(**) Laboratoire de Génétique Moléculaire, École Normale Supérieure\n\n"
+          + "Copyright 2006 École Normale Supérieure.\n" + LICENCE_TXT + "\n";
 
-  public static final String ABOUT_HTML = "<p><b>"
-      + Globals.APP_NAME
-      + " version "
-      + Globals.APP_VERSION
-      + " ("
-      + Globals.APP_BUILD_NUMBER
-      + ")"
-      + "</b> is a software to calc the distances between "
-      + "mitochondria and mRNA.</p><br/>"
-      + "<br/>This version has been built on "
-      + APP_BUILD_DATE
-      + ".<br/><br/>"
-      + "<b>Authors</b>:"
-      + "<ul><li><a href=\"mailto:jourdren@biologie.ens.fr?subject=CorsenSwing\">Laurent Jourdren</a><br/>"
-      + "<a href=\"http://transcriptome.ens.fr\">Microarray platform, École Normale Supérieure</a>"
-      + "<br/>Main developer, maintener.</li><br/>"
-      + "<li><a href=\"mailto:garcia@biologie.ens.fr?subject=CorsenSwing\">Mathilde Garcia</a><br/>"
-      + "<a href=\"http://www.biologie.ens.fr/lgmgml\">Laboratoire de Génétique Moléculaire, École Normale Supérieure</a>"
-      + "<br/>Project leader, R programming, ImageJ scripting, testing.</li></ul>"
-      + "<p>Copyright 2006 École Normale Supérieure.<br/>" + LICENCE_TXT
-      + "</p>";
+  public static final String ABOUT_HTML =
+      "<p><b>"
+          + Globals.APP_NAME
+          + " version "
+          + Globals.APP_VERSION
+          + " ("
+          + Globals.APP_BUILD_NUMBER
+          + ")"
+          + "</b> is a software to calc the distances between "
+          + "mitochondria and mRNA.</p><br/>"
+          + "<br/>This version has been built on "
+          + APP_BUILD_DATE
+          + ".<br/><br/>"
+          + "<b>Authors</b>:"
+          + "<ul><li><a href=\"mailto:jourdren@biologie.ens.fr?subject=CorsenSwing\">Laurent Jourdren</a><br/>"
+          + "<a href=\"http://transcriptome.ens.fr\">Microarray platform, École Normale Supérieure</a>"
+          + "<br/>Main developer, maintener.</li><br/>"
+          + "<li><a href=\"mailto:garcia@biologie.ens.fr?subject=CorsenSwing\">Mathilde Garcia</a><br/>"
+          + "<a href=\"http://www.biologie.ens.fr/lgmgml\">Laboratoire de Génétique Moléculaire, École Normale Supérieure</a>"
+          + "<br/>Project leader, R programming, ImageJ scripting, testing.</li></ul>"
+          + "<p>Copyright 2006 École Normale Supérieure.<br/>" + LICENCE_TXT
+          + "</p>";
 
   private static String getVersion() {
 
-    String s = Globals.class.getPackage().getSpecificationVersion();
+    String s = getManifestProperty("Specification-Version");
 
     return s != null ? s : "UNKNOWN VERSION";
   }
 
   private static String getBuiltNumber() {
 
-    String s = Globals.class.getPackage().getImplementationVersion();
+    String s = getManifestProperty("Implementation-Version");
 
     return s != null ? s : "UNKNOWN BUILT";
   }
@@ -160,43 +175,14 @@ public final class Globals {
     if (propertyKey == null)
       return null;
 
-    try {
-      java.net.URL codeBase = Globals.class.getProtectionDomain()
-          .getCodeSource().getLocation();
+    readManifest();
 
-      if (codeBase.getPath().endsWith(".jar")) {
-        java.util.jar.JarInputStream jin = new java.util.jar.JarInputStream(
-            codeBase.openStream());
-        java.util.jar.Manifest mf = jin.getManifest();
-
-        java.util.Map entries = mf.getMainAttributes();
-
-        for (java.util.Iterator it = entries.keySet().iterator(); it.hasNext();) {
-
-          java.util.jar.Attributes.Name key = (java.util.jar.Attributes.Name) it
-              .next();
-
-          if (propertyKey.equals(key.toString())) {
-            return (String) entries.get(key);
-
-          }
-        }
-
-        jin.close();
-      }
-    } catch (Exception e) {
-      return null;
-    }
-
-    return null;
+    return manifestProperties.getProperty(propertyKey);
   }
 
   private static final boolean isJar() {
 
-    java.net.URL codeBase = Globals.class.getProtectionDomain().getCodeSource()
-        .getLocation();
-
-    return codeBase.getPath().endsWith(".jar");
+    return manifestProperties.size() != 0;
   }
 
   /**
@@ -220,4 +206,24 @@ public final class Globals {
 
     return sb.toString();
   }
+
+  private static void readManifest() {
+
+    if (manifestProperties != null)
+      return;
+
+    try {
+      manifestProperties = new Properties();
+
+      InputStream is =
+          Globals.class.getResourceAsStream(MANIFEST_PROPERTIES_FILE);
+
+      if (is == null)
+        return;
+
+      manifestProperties.load(is);
+    } catch (IOException e) {
+    }
+  }
+
 }
