632d0a50eb3c34368e1ff48182e5c58f3c1d46be
Laurent Jourdren <jourdren@biologie.ens.fr>
2007-12-17 10:29:02 +0000

    Draw a Dice.

--- src/main/java/fr/ens/transcriptome/corsen/gui/qt/TestGL.java
+++ src/main/java/fr/ens/transcriptome/corsen/gui/qt/TestGL.java
@@ -22,10 +22,14 @@
 
 package fr.ens.transcriptome.corsen.gui.qt;
 
+import java.nio.DoubleBuffer;
+import java.nio.FloatBuffer;
+
 import javax.media.opengl.GL;
 import javax.media.opengl.GLContext;
 import javax.media.opengl.GLDrawableFactory;
 import javax.media.opengl.glu.GLU;
+import javax.media.opengl.glu.GLUquadric;
 
 import com.sun.opengl.util.GLUT;
 import com.trolltech.qt.core.QPoint;
@@ -56,14 +60,26 @@ public class TestGL extends QGLWidget {
 
   private static final double MAX_ZOOM = 2;
 
+  public static final int STACKS = 20;
+  private static final float LEN = 0.5f;
+  private boolean remakeObject = true;
+
   QPoint lastPos;
 
   private int gllist = -1;
 
+  /**
+   * Test if the particles can be drawed
+   * @return Returns the remakeObject
+   */
+  public boolean isRemakeObject() {
+    return remakeObject;
+  }
+
   public void initializeGL() {
 
-    GLContext context = GLDrawableFactory.getFactory()
-        .createExternalGLContext();
+    GLContext context =
+        GLDrawableFactory.getFactory().createExternalGLContext();
 
     this.gl = context.getGL();
 
@@ -71,47 +87,67 @@ public class TestGL extends QGLWidget {
     this.glu = new GLU();
     this.glut = new GLUT();
 
-    // glu.gluPerspective(40.0,1.5,50.0,100.0);
-    // glu.gluLookAt(-100, 100, 100, 100, 100, 25, 0, 1, 0);
+    FloatBuffer mat_specular = FloatBuffer.wrap(new float[] {1, 1, 1, 1});
+    FloatBuffer mat_shininess = FloatBuffer.wrap(new float[] {50});
 
-    // qglClearColor(trolltechPurple.dark());
+    FloatBuffer white_light = FloatBuffer.wrap(new float[] {1, 1, 1, 1});
+    FloatBuffer lmodel_ambient =
+        FloatBuffer.wrap(new float[] {0.9f, 0.9f, 0.9f, 1.0f});
+
+    // FloatBuffer light_ambient = FloatBuffer.wrap(new float[] {.9f, .9f, .9f,
+    // 1});
+    FloatBuffer light_ambient =
+        FloatBuffer.wrap(new float[] {.3f, .3f, .3f, 3f});
+    FloatBuffer light_diffuse = FloatBuffer.wrap(new float[] {1, 1, 1, 1});
+    FloatBuffer light_specular = FloatBuffer.wrap(new float[] {1, 1, 1, 1});
+    FloatBuffer light_position0 = FloatBuffer.wrap(new float[] {1, 1, 1, 0});
+    FloatBuffer light_position1 = FloatBuffer.wrap(new float[] {-1, -1, -1, 0});
+
+    gl.glClearColor(0, 0, 0, 0);
+    gl.glMaterialfv(GL.GL_FRONT, GL.GL_SPECULAR, mat_specular);
+    gl.glMaterialfv(GL.GL_FRONT, GL.GL_SHININESS, mat_shininess);
+    gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, light_position0);
+    gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, white_light);
+    gl.glLightfv(GL.GL_LIGHT0, GL.GL_SPECULAR, white_light);
+
+    gl.glLightfv(GL.GL_LIGHT0, GL.GL_AMBIENT, light_ambient);
+    gl.glLightfv(GL.GL_LIGHT0, GL.GL_DIFFUSE, light_diffuse);
+    gl.glLightfv(GL.GL_LIGHT0, GL.GL_SPECULAR, light_specular);
+    gl.glLightfv(GL.GL_LIGHT0, GL.GL_POSITION, light_position0);
+
+    gl.glLightf(GL.GL_LIGHT0, GL.GL_LINEAR_ATTENUATION, 1.0f);
+    gl.glLightf(GL.GL_LIGHT0, GL.GL_CONSTANT_ATTENUATION, 2.0f);
+
+    gl.glLightfv(GL.GL_LIGHT1, GL.GL_POSITION, light_position1);
+    gl.glLightfv(GL.GL_LIGHT1, GL.GL_DIFFUSE, white_light);
+    gl.glLightfv(GL.GL_LIGHT1, GL.GL_SPECULAR, white_light);
+
+    gl.glLightfv(GL.GL_LIGHT1, GL.GL_AMBIENT, light_ambient);
+    gl.glLightfv(GL.GL_LIGHT1, GL.GL_DIFFUSE, light_diffuse);
+    gl.glLightfv(GL.GL_LIGHT1, GL.GL_SPECULAR, light_specular);
+    gl.glLightfv(GL.GL_LIGHT1, GL.GL_POSITION, light_position1);
+
+    gl.glLightf(GL.GL_LIGHT1, GL.GL_LINEAR_ATTENUATION, 1.0f);
+    gl.glLightf(GL.GL_LIGHT1, GL.GL_CONSTANT_ATTENUATION, 2.0f);
+
+    gl.glLightModelfv(GL.GL_LIGHT_MODEL_AMBIENT, lmodel_ambient);
+
+    gl.glEnable(GL.GL_LIGHTING);
+    gl.glEnable(GL.GL_LIGHT0);
+    gl.glEnable(GL.GL_LIGHT1);
+    gl.glEnable(GL.GL_DEPTH_TEST);
+
+    this.gl.glMatrixMode(GL.GL_PROJECTION);
     makeObject();
 
     this.gl.glShadeModel(GL.GL_FLAT);
-    // this.gl.glEnable(GL.GL_DEPTH_TEST);
-    // this.gl.glEnable(GL.GL_CULL_FACE);
-
-    // Lights parameters
-
-    /*
-     * this.gl.glShadeModel(GL.GL_SMOOTH);
-     * this.gl.glLightModeli(GL.GL_LIGHT_MODEL_LOCAL_VIEWER, GL.GL_TRUE);
-     * this.gl.glEnable(GL.GL_LIGHTING); this.gl.glEnable(GL.GL_LIGHT0);
-     * FloatBuffer l0dif = FloatBuffer.wrap(new float[] {0.3f,0.3f,0.8f});
-     * FloatBuffer l0pos = FloatBuffer.wrap(new float[] {0f,0f,0f}); FloatBuffer
-     * l1dif = FloatBuffer.wrap(new float[] {0.5f,0.5f,0.5f}); FloatBuffer mSpec =
-     * FloatBuffer.wrap(new float[] {0.5f,0.5f,0.5f});
-     */
-
-    // this.gl.glEnable(GL.GL_LIGHT1);
-    // this.gl.glLightfv(GL.GL_LIGHT0,GL.GL_POSITION, l0dif);
-    /*
-     * this.gl.glLightfv(GL.GL_LIGHT0,GL.GL_DIFFUSE, l0dif);
-     * this.gl.glLightfv(GL.GL_LIGHT0,GL.GL_SPECULAR, l0dif);
-     * this.gl.glLightfv(GL.GL_LIGHT1,GL.GL_DIFFUSE, l1dif);
-     * this.gl.glLightfv(GL.GL_LIGHT1,GL.GL_SPECULAR, l1dif);
-     */
-
-    // Materials
-    // this.gl.glMaterialfv(GL.GL_FRONT_AND_BACK, GL.GL_SPECULAR, mSpec);
-    // this.gl.glMaterialf(GL.GL_FRONT_AND_BACK, GL.GL_SHININESS, 50);
   }
 
   public void resizeGL(int width, int height) {
 
     int side = width <= height ? width : height; // qMin(width, height);
     this.gl.glViewport((width - side) / 2, (height - side) / 2, side, side);
-    //this.gl.glViewport(0, 0, width, height);
+    // this.gl.glViewport(0, 0, width, height);
 
     this.gl.glMatrixMode(GL.GL_PROJECTION);
     this.gl.glLoadIdentity();
@@ -129,7 +165,15 @@ public class TestGL extends QGLWidget {
     this.gl.glRotated(yRot / 16.0, 0.0, 1.0, 0.0);
     this.gl.glRotated(zRot / 16.0, 0.0, 0.0, 1.0);
 
-    makeObject();
+    if (isRemakeObject()) {
+      makeObject();
+      this.remakeObject = false;
+      // setOkToDraw(false);
+    }
+
+    this.gl.glMatrixMode(GL.GL_MODELVIEW);
+    this.gl.glCallList(this.gllist);
+    this.gl.glFlush();
 
     this.gl.glMatrixMode(GL.GL_PROJECTION);
     this.gl.glLoadIdentity();
@@ -226,29 +270,209 @@ public class TestGL extends QGLWidget {
     repaint();
   }
 
-  private void makeObject() {
+  private void makeObjectSphere(final float x, float y, float z, int xQuadrant,
+      int yQuadrant, int zQuadrant) {
 
     if (this.gl == null)
       return;
 
-    GL gl = this.gl;
+    final GL gl = this.gl;
 
-    //this.glut.glutWireCube(1.0f);
+    DoubleBuffer eqnX =
+        DoubleBuffer.wrap(new double[] {xQuadrant, 0.0, 0.0, 0.0}); // x <
+    // 0
 
-    //this.glu.gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
+    DoubleBuffer eqnY =
+        DoubleBuffer.wrap(new double[] {0.0, yQuadrant, 0.0, 0.0}); // y <
+    // 0
 
-    gl.glTranslated(-.25,- .25, 0);
-    
-    gl.glColor3f(1.0f, 1.0f, 1.0f);
-    gl.glBegin(GL.GL_POLYGON);
-    gl.glVertex3f(.25f, .25f, 0);
-    gl.glVertex3f(.75f, .25f, 0);
-    gl.glVertex3f(.75f, .75f, 0);
-    gl.glVertex3f(.25f, .75f, 0);
-    gl.glEnd();
+    DoubleBuffer eqnZ =
+        DoubleBuffer.wrap(new double[] {0.0, 0.0, zQuadrant, 0.0}); // y <
+    // 0
+
+    gl.glPushMatrix();
+
+    gl.glTranslatef(x, y, z);
+
+    gl.glClipPlane(GL.GL_CLIP_PLANE0, eqnX);
+    gl.glEnable(GL.GL_CLIP_PLANE0);
+    gl.glClipPlane(GL.GL_CLIP_PLANE1, eqnY);
+    gl.glEnable(GL.GL_CLIP_PLANE1);
+    gl.glClipPlane(GL.GL_CLIP_PLANE2, eqnZ);
+    gl.glEnable(GL.GL_CLIP_PLANE2);
+
+    this.glu.gluSphere(glu.gluNewQuadric(), LEN, STACKS, STACKS);
+    // this.glut.glutSolidSphere(LEN, STACKS, STACKS);
+
+    gl.glDisable(GL.GL_CLIP_PLANE0);
+    gl.glDisable(GL.GL_CLIP_PLANE1);
+    gl.glDisable(GL.GL_CLIP_PLANE2);
+
+    gl.glPopMatrix();
 
     gl.glFlush();
+  }
 
+  private void makeObjectCylinder(final float x, float y, float z,
+      int xQuadrant, int yQuadrant, int zQuadrant, final int sens) {
+
+    if (this.gl == null)
+      return;
+
+    final GL gl = this.gl;
+
+    DoubleBuffer eqnX =
+        DoubleBuffer.wrap(new double[] {xQuadrant, 0.0, 0.0, 0.0}); // x <
+    // 0
+
+    DoubleBuffer eqnY =
+        DoubleBuffer.wrap(new double[] {0.0, yQuadrant, 0.0, 0.0}); // y <
+    // 0
+
+    DoubleBuffer eqnZ =
+        DoubleBuffer.wrap(new double[] {0.0, 0.0, zQuadrant, 0.0}); // y <
+    // 0
+
+    gl.glPushMatrix();
+
+    gl.glTranslatef(x, y, z);
+
+    if (sens == 1)
+      gl.glRotatef(90, 0, 1, 0);
+    else if (sens == 2)
+      gl.glRotatef(90f, 1, 0, 0);
+
+    gl.glClipPlane(GL.GL_CLIP_PLANE0, eqnX);
+    gl.glEnable(GL.GL_CLIP_PLANE0);
+    gl.glClipPlane(GL.GL_CLIP_PLANE1, eqnY);
+    gl.glEnable(GL.GL_CLIP_PLANE1);
+    gl.glClipPlane(GL.GL_CLIP_PLANE2, eqnZ);
+    gl.glEnable(GL.GL_CLIP_PLANE2);
+
+    final int stacks = sens == 0 ? STACKS : STACKS * 2;
+
+    this.glu.gluCylinder(glu.gluNewQuadric(), LEN, LEN, LEN, stacks, stacks);
+    // this.glut.glutSolidCylinder(LEN, LEN, stacks, stacks);
+
+    gl.glDisable(GL.GL_CLIP_PLANE0);
+    gl.glDisable(GL.GL_CLIP_PLANE1);
+    gl.glDisable(GL.GL_CLIP_PLANE2);
+
+    gl.glPopMatrix();
+
+    gl.glFlush();
+  }
+
+  private static final float[][] boxNormals =
+      { {-1.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f, 0.0f},
+          {0.0f, -1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f, -1.0f}};
+
+  private void makeObjectSquare(final float x, float y, float z, final int sens) {
+
+    if (this.gl == null)
+      return;
+
+    final GL gl = this.gl;
+
+    gl.glPushMatrix();
+    gl.glTranslatef(x, y, z);
+
+    if (sens == 1)
+      gl.glRotatef(90, 0, 1, 0);
+    else if (sens == 2)
+      gl.glRotatef(90, 1, 0, 0);
+
+    gl.glNormal3fv(new float[] {0.0f, 0.0f, -1.0f}, 0);
+
+    gl.glBegin(GL.GL_POLYGON);
+
+    gl.glVertex3f(0, 0, 0);
+    gl.glVertex3f(0, LEN, 0);
+    gl.glVertex3f(LEN, LEN, 0);
+    gl.glVertex3f(LEN, 0, 0);
+
+    gl.glEnd();
+    gl.glPopMatrix();
+
+    gl.glFlush();
+  }
+
+  private void makeObject() {
+
+    if (gllist > 0)
+      this.gl.glDeleteLists(gllist, 1);
+    int list = this.gl.glGenLists(++gllist);
+
+    this.gl.glNewList(list, GL.GL_COMPILE);
+
+    // makeObjectCylinder2(0, 0, 0, 1, 1, 1);
+    // makeObjectCylinder2(-1,-1, -1, 1, 1, 1);
+    // makeObjectCylinder2(1,1, 1, 1, 1, 1);
+
+    // makeObjectSphere(0, 0.5f, 0);
+    // makeObjectSquare(0, 0, 0);
+
+    // First level
+
+    makeObjectSphere(0, LEN, LEN, 1, 1, 1);
+
+    makeObjectSquare(-LEN, 0, LEN * 2, 0);
+
+    makeObjectCylinder(-LEN, LEN, LEN, -1, 1, 1, 1);
+    makeObjectSphere(-LEN, LEN, LEN, -1, 1, 1);
+    makeObjectCylinder(0, LEN, LEN, 1, 1, 1, 2);
+    makeObjectCylinder(-LEN, LEN, LEN, -1, 1, 1, 2);
+    makeObjectCylinder(-LEN, 0, LEN, -1, -1, 1, 1);
+    makeObjectSphere(0, 0, LEN, 1, -1, 1);
+    makeObjectSphere(-LEN, 0, LEN, -1, -1, 1);
+
+    // Second level
+
+    makeObjectSquare(-2 * LEN, 0, LEN, 1);
+    makeObjectSquare(LEN, 0, LEN, 1);
+    makeObjectSquare(-LEN, -LEN, 0, 2);
+    makeObjectSquare(-LEN, 2 * LEN, 0, 2);
+
+    makeObjectCylinder(0, LEN, 0, 1, 1, 1, 0);
+    makeObjectCylinder(-LEN, LEN, 0, -1, 1, 1, 0);
+    makeObjectCylinder(0, 0, 0, 1, -1, 1, 0);
+    makeObjectCylinder(-LEN, 0, 0, -1, -1, 1, 0);
+
+    // Third level
+
+    makeObjectSphere(0, LEN, 0, 1, 1, -1);
+    makeObjectCylinder(-LEN, LEN, 0, 1, 1, 1, 1);
+    makeObjectSphere(-LEN, LEN, 0, -1, 1, -1);
+    makeObjectCylinder(0, LEN, 0, 1, -1, 1, 2);
+    makeObjectSphere(0, 0, 0, 1, -1, -1);
+
+    makeObjectCylinder(-LEN, LEN, 0, -1, -1, 1, 2);
+
+    makeObjectCylinder(-LEN, 0, 0, 1, -1, 1, 1);
+    makeObjectSphere(-LEN, 0, 0, -1, -1, -1);
+
+    makeObjectSquare(-LEN, 0, -LEN, 0);
+
+    gl.glFlush();
+    gl.glEndList();
+    System.out.println(gllist);
+
+  }
+
+  private DoubleBuffer eqn =
+      DoubleBuffer.wrap(new double[] {0.0, 1.0, 0.0, 0.0}); // y < 0
+
+  private DoubleBuffer eqn3 =
+      DoubleBuffer.wrap(new double[] {0.0, 0.0, 1.0, 1.0}); // x < 0
+  private float spin = 0;
+
+  private GLUquadric sphereQuadratic;// = glu.gluNewQuadric();
+
+  private void SpinDisplay() {
+
+    this.spin = spin + 2.0f;
+    if (this.spin > 360.0)
+      this.spin = spin - 360.0f;
   }
 
   public static void main(String[] args) {
